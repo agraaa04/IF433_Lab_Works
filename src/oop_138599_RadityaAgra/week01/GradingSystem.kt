9f0e16b0b22d70d3868142fdd3ed863a37b7fccc
RAAGRA <radityaagra045@gmail.com>
2026-02-04 17:21:06 +0700

    oop_138599_RadityaAgra.week01: implement null safety check

--- src/oop_138599_RadityaAgra/week01/GradingSystem.kt
+++ src/oop_138599_RadityaAgra/week01/GradingSystem.kt
@@ -16,6 +16,14 @@ fun main() {
     }
 
     println("Grade kamu: $grade")
+
+    //Tambahkan di akhir main()
+    val studentId: String? = null
+
+    //Jika null, gunakan nilai default 0
+    val idLength = studentId?.length ?: 0
+
+    println("Panjang ID: $idLength")
 }
 
 fun calculateStatus(score: Int) = if (score > 75) "Lulus" else "Tidak Lulus"
