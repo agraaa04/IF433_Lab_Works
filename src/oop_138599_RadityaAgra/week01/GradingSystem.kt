6a49c25d7c867982e1742e162e2fdf7fe1e2e38f
RAAGRA <radityaagra045@gmail.com>
2026-02-04 17:10:15 +0700

    oop_138599_RadityaAgra.week01: initial setup and procedural code

---
+++ src/oop_138599_RadityaAgra/week01/GradingSystem.kt
@@ -0,0 +1,10 @@
+package oop_138599_RadityaWeek01.week01
+
+fun main() {
+    var name: String = "John Thor"
+    var score: Int = 80
+
+    // Concattenation gaya lama
+    println("Name " + name + " Nilai " + score)
+
+}
