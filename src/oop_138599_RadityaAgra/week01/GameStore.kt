7c4c8465df410fc4113782b4ba16adfe8cca64e5
RAAGRA <radityaagra045@gmail.com>
2026-02-10 00:34:49 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_138599_RadityaAgra/week01/GameStore.kt
+++ src/oop_138599_RadityaAgra/week01/GameStore.kt
@@ -3,4 +3,13 @@ package oop_138599_RadityaAgra.week01
 fun main() {
     val gameTitle = "Elden Ring"
     val price = 750_000
+
+    val discount = calculateDiscount(price)
 }
+
+fun calculateDiscount(price: Int): Int =
+    if (price > 500_000) {
+        price * 20 / 100
+    } else {
+        price * 10 / 100
+    }
