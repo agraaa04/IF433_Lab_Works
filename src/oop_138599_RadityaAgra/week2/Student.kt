6959ec136ed40255b870c8587f092cb6d65c71ec
RAAGRA <radityaagra045@gmail.com>
2026-02-11 15:21:48 +0700

    week02: add warning validation in init block

--- src/oop_138599_RadityaAgra/week2/Student.kt
+++ src/oop_138599_RadityaAgra/week2/Student.kt
@@ -1,9 +1,13 @@
 package oop_138599_RadityaAgra.week2
 
-class Student (
-    val name:String,
-    val nim:String,
-    val major:String,
-){
-
+class Student (val name:String,val nim:String, val major:String){
+    init{
+        //Validasi sederhana: Cek panjang NIM
+        if (nim.length != 5){
+            println("WARNING: Object tercipta dengan NIM ($nim) yang tidak tidak valid")
+            println("Data mahasiswa $name mungkin akan bermasalah di sistem")
+        }else {
+            println("LOG: Object Student $name berhasil dialokasikan di Memory")
+        }
+    }
 }
