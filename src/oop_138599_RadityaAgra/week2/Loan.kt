874e627c9c7ff7a18bcb97886a42c06e6edb2380
RAAGRA <radityaagra045@gmail.com>
2026-02-16 16:24:17 +0700

    week02: (task 1) create Loan class with default args

---
+++ src/oop_138599_RadityaAgra/week2/Loan.kt
@@ -0,0 +1,13 @@
+package oop_138599_RadityaAgra.week02
+
+class Loan(
+    val bookTitle: String,
+    val borrower: String,
+    val loanDuration: Int = 1
+) {
+    fun calculateFine(): Int {
+        return if (loanDuration > 3)
+            (loanDuration - 3) * 2000
+        else 0
+    }
+}
