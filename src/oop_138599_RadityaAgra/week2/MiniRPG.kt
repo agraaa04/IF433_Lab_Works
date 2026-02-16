acfc7e672c2330add9ca3016f2cd762ddd4b2c85
RAAGRA <radityaagra045@gmail.com>
2026-02-16 17:17:56 +0700

    week02: (task 2) implement the game

--- src/oop_138599_RadityaAgra/week2/MiniRPG.kt
+++ src/oop_138599_RadityaAgra/week2/MiniRPG.kt
@@ -1,4 +1,5 @@
 package oop_138599_RadityaAgra.week02
+import java.util.Scanner
 
 class Hero(
     val name: String,
@@ -19,3 +20,45 @@ class Hero(
         return hp > 0
     }
 }
+
+fun main(){
+    val scanner = Scanner(System.`in`)
+
+    print("Nama Hero: ")
+    val heroName = scanner.nextLine()
+
+    print("Damage Hero: ")
+    val damage = scanner.nextInt()
+
+    val hero = Hero(heroName, damage)
+    var enemyHp = 100
+
+    while (hero.isAlive() && enemyHp > 0) {
+        println("1. Serang")
+        println("2. Kabur")
+
+        val choice = scanner.nextInt()
+
+        if (choice == 1) {
+            enemyHp -= hero.baseDamage
+            if (enemyHp < 0) enemyHp = 0
+            println("HP Musuh: $enemyHp")
+
+            if (enemyHp > 0) {
+                val enemyDamage = (10..20).random()
+                hero.takeDamage(enemyDamage)
+                println("HP Hero: ${hero.hp}")
+            }
+
+        } else {
+            println("Hero kabur!")
+            break
+        }
+    }
+
+    if (hero.isAlive() && enemyHp == 0)
+        println("Hero Menang!")
+    else if (!hero.isAlive())
+        println("Hero Kalah!")
+}
+
