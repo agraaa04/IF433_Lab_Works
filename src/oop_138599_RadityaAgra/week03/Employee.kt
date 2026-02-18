2f09b4dded34a49a9507ef4cc1fe24dc6ffdef86
RAAGRA <radityaagra045@gmail.com>
2026-02-18 15:23:19 +0700

    week03: test all encapsulation logic in main

--- src/oop_138599_RadityaAgra/week03/Employee.kt
+++ src/oop_138599_RadityaAgra/week03/Employee.kt
@@ -21,10 +21,24 @@ class Employee (val name:String) {
         println("Karyawan: $name, Rating: $performanceRating")
     }
 
+    //Tax (Pajak) dihitung 10% dari gaji saat ini
+    val tax: Double
+        get() = salary * 0.1
+
 }
 
 fun main(){
     val e = Employee("Budi")
-    println("--- Test Error ---")
-    e.salary = 5000
+
+    //1. Test Validasi Salary
+    e.salary = -1000 // Harusnya print Error
+    e.salary = 5000000
+    println("Gaji: ${e.salary}")
+
+    //2. test Encapsulation
+    e.increasePerformance()
+//  e.performarating = 5 // Coba uncoment, pasti Merah (error)
+
+    //3. Test Computed Property
+    println("Pajak yang harus dinayar: ${e.tax}")
 }
