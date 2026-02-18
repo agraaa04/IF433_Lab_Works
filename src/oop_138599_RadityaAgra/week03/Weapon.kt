f2b5888327582080d3c36465d9313825eb2acd8f
RAAGRA <radityaagra045@gmail.com>
2026-02-18 15:36:34 +0700

    week03: (task 1) create Weapon class with validation

---
+++ src/oop_138599_RadityaAgra/week03/Weapon.kt
@@ -0,0 +1,21 @@
+package oop_138599_RadityaAgra.week03
+
+class Weapon(val name:String, damage: Int){
+    var damage: Int = damage
+        set(value) {
+            if (value < 0) {
+                println("Damage tidak boleh negatif!")
+            } else if (value > 1000) {
+                field = 1000
+            } else {
+                field = value
+            }
+        }
+
+    val tier: String
+        get() = when {
+            damage > 800 -> "Legendary"
+            damage > 500 -> "Epic"
+            else -> "Common"
+        }
+}
