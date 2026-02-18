2c6b726c3be89a91324025f3ead2f5b642c282e3
RAAGRA <radityaagra045@gmail.com>
2026-02-18 15:24:06 +0700

    week03: cleanup code

--- src/oop_138599_RadityaAgra/week03/Employee.kt
+++ src/oop_138599_RadityaAgra/week03/Employee.kt
@@ -21,7 +21,6 @@ class Employee (val name:String) {
         println("Karyawan: $name, Rating: $performanceRating")
     }
 
-    //Tax (Pajak) dihitung 10% dari gaji saat ini
     val tax: Double
         get() = salary * 0.1
 
@@ -30,15 +29,11 @@ class Employee (val name:String) {
 fun main(){
     val e = Employee("Budi")
 
-    //1. Test Validasi Salary
-    e.salary = -1000 // Harusnya print Error
+    e.salary = -1000
     e.salary = 5000000
     println("Gaji: ${e.salary}")
 
-    //2. test Encapsulation
     e.increasePerformance()
-//  e.performarating = 5 // Coba uncoment, pasti Merah (error)
 
-    //3. Test Computed Property
     println("Pajak yang harus dinayar: ${e.tax}")
 }
