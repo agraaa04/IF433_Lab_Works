fa2f928ff8422d05eacd5d08a1256267c3051392
RAAGRA <radityaagra045@gmail.com>
2026-02-18 15:14:47 +0700

    week03: fix setter using backing field

--- src/oop_138599_RadityaAgra/week03/Employee.kt
+++ src/oop_138599_RadityaAgra/week03/Employee.kt
@@ -1,15 +1,19 @@
 package oop_138599_RadityaAgra.week03
 
+class Employee (val name:String) {
+    var salary: Int = 0
+        set(value) {
+            if (value < 0){
+                println("Error: Gaji tidak boleh negatif! DI-set ke 0.")
+                field = 0
+            }else {
+                field = value
+            }
+        }
+}
+
 fun main(){
     val e = Employee("Budi")
     println("--- Test Error ---")
     e.salary = 5000
-}
-
-class Employee (val name:String) {
-    var salary: Int = 0
-        set(value) {
-            println("Mencoba set gaji ke: $value")
-            this.salary = value
-        }
 }
