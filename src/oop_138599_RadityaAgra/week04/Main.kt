11d2eb2d85779fa08e79e5307398c647061dfff0
RAAGRA <radityaagra045@gmail.com>
2026-02-25 14:56:12 +0700

    week04: test Vehicle and Car in main

---
+++ src/oop_138599_RadityaAgra/week04/Main.kt
@@ -0,0 +1,16 @@
+package oop_138599_RadityaAgra.week04
+
+fun main() {
+    println("--- Testing Vehicle ---")
+    val generalVehicle = Vehicle("Sepeda Onthel")
+    generalVehicle.honk()
+    generalVehicle.accelerate()
+
+    println()
+
+    println("--- Testing Vehicle ---")
+    val myCar = Car("Honda", 4)
+    myCar.openTrunk()
+    myCar.honk()
+    myCar.accelerate()
+}
