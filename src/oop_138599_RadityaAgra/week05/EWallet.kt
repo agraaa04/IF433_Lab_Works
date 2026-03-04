46a82dfffba089a623c9e09e2b80218912982563
RAAGRA <radityaagra045@gmail.com>
2026-03-04 15:12:25 +0700

    week5: (task 2) create EWallet class

---
+++ src/oop_138599_RadityaAgra/week05/EWallet.kt
@@ -0,0 +1,19 @@
+package oop_138599_RadityaAgra.week05
+
+class EWallet(accountName: String, var balance: Double)
+    : PaymentMethod(accountName) {
+
+    override fun processPayment(amount: Double) {
+        if (balance >= amount) {
+            balance -= amount
+            println("Pembayaran berhasil dengan E-Wallet. Sisa saldo: $balance")
+        } else {
+            println("Saldo tidak cukup.")
+        }
+    }
+
+    fun topUp(amount: Double) {
+        balance += amount
+        println("Top up berhasil. Saldo sekarang: $balance")
+    }
+}
