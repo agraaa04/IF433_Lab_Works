baba5322c327630e9f862a2bce098905c55af931
RAAGRA <radityaagra045@gmail.com>
2026-03-11 16:06:12 +0700

    week6: fix diamond problem using explicit super delegation

--- src/oop_138599_RadityaAgra/week06/Smartphone.kt
+++ src/oop_138599_RadityaAgra/week06/Smartphone.kt
@@ -3,4 +3,9 @@ package oop_138599_RadityaAgra.week06
 // ERROR: Class Smartphone inherits multiple implementations of turnon()
 
 class Smartphone: Camera, Phone {
+    override fun turnon() {
+        super<Camera>.turnon()
+        super<Phone>.turnon()
+        println("Sistem operasi Smartphone berhasil booting.")
+    }
 }
