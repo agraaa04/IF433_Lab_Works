2624d13b00ac0f5faf4a1c156a77cfdca73da021
RAAGRA <radityaagra045@gmail.com>
2026-03-25 15:52:18 +0700

    week7:  (task) create BattleState sealed class hierarchy

---
+++ src/oop_138599_RadityaAgra/week07/BattleEvent.kt
@@ -0,0 +1,8 @@
+package oop_138599_RadityaAgra.week07
+
+sealed class BattleState {
+    data class MonsterEncounter(val monsterName: String): BattleState()
+    data class LootDropped(val item: GameItem): BattleState()
+    data class GameOver(val reason: String): BattleState()
+    object SafeZone: BattleState()
+}
