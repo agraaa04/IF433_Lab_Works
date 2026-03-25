afe185a81d4331c23a92150f69dad0862ef76cf4
RAAGRA <radityaagra045@gmail.com>
2026-03-25 15:49:24 +0700

    week7:  (task) create GameItem data class

--- src/oop_138599_RadityaAgra/week07/GameModels.kt
+++ src/oop_138599_RadityaAgra/week07/GameModels.kt
@@ -3,3 +3,5 @@ package oop_138599_RadityaAgra.week07
 enum class ItemRarity(val dropChance: Int) {
     COMMON(70), UNCOMMON(30), RARE(15), EPIC(5), LEGENDARY(1)
 }
+
+data class GameItem(val name: String, val damage: Int, val rarity: ItemRarity)
