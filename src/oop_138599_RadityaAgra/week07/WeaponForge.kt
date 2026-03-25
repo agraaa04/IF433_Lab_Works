ab2f333cbd8dfd5256a89ab223834b165fa4fc5d
RAAGRA <radityaagra045@gmail.com>
2026-03-25 15:51:01 +0700

    week7:  (task) implement factory pattern in WeaponForge

---
+++ src/oop_138599_RadityaAgra/week07/WeaponForge.kt
@@ -0,0 +1,12 @@
+package oop_138599_RadityaAgra.week07
+
+class Weapon private constructor(val item: GameItem, val durability: Int) {
+    companion object {
+        fun forgeStarterSword(): Weapon {
+            return Weapon(GameItem("Pedang Kayu Bapuk", 5, ItemRarity.COMMON), 50)
+        }
+        fun forgeEpicSword(): Weapon {
+            return Weapon(GameItem("Excalibur", 100, ItemRarity.EPIC), 200)
+        }
+    }
+}
