808d5ca5894b106d710355ddee45fde4aede841a
RAAGRA <radityaagra045@gmail.com>
2026-04-15 16:22:59 +0700

    week08: (task) create sealed class Product and data classes

--- src/oop_138599_RadityaAgra/week08/EcommerceModel.kt
+++ src/oop_138599_RadityaAgra/week08/EcommerceModel.kt
@@ -1,15 +1,15 @@
 package oop_138599_RadityaAgra.week08
 
-sealed class Product [cite: 192]
+sealed class Product
 
 data class Electronic(
     val id: String,
     val name: String,
     val warrantyMonths: Int
-) : Product() [cite: 193]
+) : Product()
 
 data class Clothing(
     val id: String,
     val name: String,
     val size: String
-) : Product() [cite: 194]
+) : Product()
---
+++ src/oop_138599_RadityaAgra/week08/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_138599_RadityaAgra.week08;
+
+public class JavaPaymentService { [cite: 210]
+    public static String processPayment(String productId) { [cite: 211]
+        return "TRX-" + productId + "-SUCCESS"; [cite: 212]
+    }
+}
