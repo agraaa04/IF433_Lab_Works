66a527b5d82f19c9cd9c95968b7daff458f7184f
RAAGRA <radityaagra045@gmail.com>
2026-04-15 16:28:19 +0700

    week08: (task) implement payment processing with reset

--- src/oop_138599_RadityaAgra/week08/ApiParser.kt
+++ src/oop_138599_RadityaAgra/week08/ApiParser.kt
@@ -23,4 +23,15 @@ class ApiParser {
             else -> null
         }
     }
+
+    fun checkout(product: Product) {
+        val id = when (product) {
+            is Electronic -> product.id
+            is Clothing -> product.id
+        }
+
+        // Gunakan !! pada hasil tangkapan Java service [cite: 219]
+        val transactionId = JavaPaymentService.processPayment(id)!!
+        println("Transaction ID: $transactionId")
+    }
 }
