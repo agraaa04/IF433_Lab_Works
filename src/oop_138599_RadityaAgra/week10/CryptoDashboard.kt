baee69c4062edc27c5d602d727c09154a9c39aa6
RAAGRA <radityaagra045@gmail.com>
2026-04-29 15:30:20 +0700

    week10: (task) initialize and populate transaction repository

--- src/oop_138599_RadityaAgra/week10/CryptoDashboard.kt
+++ src/oop_138599_RadityaAgra/week10/CryptoDashboard.kt
@@ -14,4 +14,13 @@ fun main() {
     response.data.forEach { coin ->
         println("Coin: ${coin.name}, Balance: ${coin.balance}")
     }
+
+    val txRepo = WalletRepository<Transaction>()
+    txRepo.add(Transaction("TX001", 0.1))
+    txRepo.add(Transaction("TX002", 50.0))
+
+    println("\n=== TRANSACTION LOG ===")
+    txRepo.getAll().forEach { tx ->
+        println("ID: ${tx.id}, Amount: ${tx.amount}")
+    }
 }
