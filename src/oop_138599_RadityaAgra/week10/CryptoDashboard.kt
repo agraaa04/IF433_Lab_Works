69524a08629904e57d375f7d5ee6ddbff3f29857
RAAGRA <radityaagra045@gmail.com>
2026-04-29 15:27:57 +0700

    week10: (task) populate coin repository

--- src/oop_138599_RadityaAgra/week10/CryptoDashboard.kt
+++ src/oop_138599_RadityaAgra/week10/CryptoDashboard.kt
@@ -2,3 +2,8 @@ package oop_138599_RadityaAgra.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 0.5))
+    coinRepo.add(Coin("ETH", 2.0))
+    coinRepo.add(Coin("USDT", 1000.0))
+}
