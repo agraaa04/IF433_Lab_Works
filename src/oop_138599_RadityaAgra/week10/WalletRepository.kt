96510fbf8ea95779ea95d0f7bad62c59885a9424
RAAGRA <radityaagra045@gmail.com>
2026-04-29 15:20:06 +0700

    week10: (task) implement generic WalletRepository

---
+++ src/oop_138599_RadityaAgra/week10/WalletRepository.kt
@@ -0,0 +1,13 @@
+package oop_138599_RadityaAgra.week10
+
+class WalletRepository<T : Any> {
+    private val items = mutableListOf<T>()
+
+    fun add(item: T) {
+        items.add(item)
+    }
+
+    fun getAll(): List<T> {
+        return items
+    }
+}
