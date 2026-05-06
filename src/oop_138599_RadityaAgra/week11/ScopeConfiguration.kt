55d30dd022732dfbe9eada5418aea5df04ae80df
RAAGRA <radityaagra045@gmail.com>
2026-05-06 14:50:28 +0700

    week11: implement apply for object initialization

--- src/oop_138599_RadityaAgra/week11/ScopeConfiguration.kt
+++ src/oop_138599_RadityaAgra/week11/ScopeConfiguration.kt
@@ -1,3 +1,13 @@
 package oop_138599_RadityaAgra.week11
 
 data class User(var name: String = "", var age: Int = 0)
+
+fun main() {
+    println("=== TEST APPLY ===")
+    // apply mengembalikan objek itu sendiri, menggunakan 'this'[cite: 3]
+    val user = User().apply {
+        name = "Alex"
+        age = 25
+    }
+    println(user)
+}
