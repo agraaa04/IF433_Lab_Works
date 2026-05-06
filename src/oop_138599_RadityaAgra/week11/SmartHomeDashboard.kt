d14855358c78fa11444a9a760729ff299f597aea
RAAGRA <radityaagra045@gmail.com>
2026-05-06 14:58:57 +0700

    week11: (task) format dashboard output using with

--- src/oop_138599_RadityaAgra/week11/SmartHomeDashboard.kt
+++ src/oop_138599_RadityaAgra/week11/SmartHomeDashboard.kt
@@ -28,4 +28,8 @@ fun main() {
     searchResult?.let {
         println(it.diagnose())
     }
+
+    with(homeDevices) {
+        println("Rangkuman Dashboard: Terdeteksi ${this.size} perangkat.")
+    }
 }
