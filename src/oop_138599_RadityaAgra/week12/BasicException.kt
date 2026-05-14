513e197a97e81db3c0218f61ae92ab404c5a32f1
RAAGRA <radityaagra045@gmail.com>
2026-05-14 21:21:16 +0700

    week12: implement try as expression

--- src/oop_138599_RadityaAgra/week12/BasicException.kt
+++ src/oop_138599_RadityaAgra/week12/BasicException.kt
@@ -1,2 +1,18 @@
 package oop_138599_RadityaAgra.week12
 
+fun divide(a: Int, b: Int): Int {
+    return try {
+        a / b
+    } catch (e: ArithmeticException) {
+        println("Error: ${e.message}")
+        -1
+    } finally {
+        println("Division attempt finished")
+    }
+}
+
+fun main() {
+    println("=== TEST BASIC EXCEPTION ===")
+    val result = divide(10, 0)
+    println("Hasil: $result")
+}
