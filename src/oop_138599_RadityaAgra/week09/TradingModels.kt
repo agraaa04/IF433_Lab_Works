b8c7b6351cfd6369388ca98b3d538ce9eb186a97
RAAGRA <radityaagra045@gmail.com>
2026-05-14 22:02:19 +0700

    week09:  (task) create TradeLog data class

---
+++ src/oop_138599_RadityaAgra/week09/TradingModels.kt
@@ -0,0 +1,9 @@
+package oop_138599_RadityaAgra.week09
+
+data class TradeLog(
+    val pair: String,
+    val position: String,
+    val leverage: Int,
+    val roe: Double,
+    val status: String
+)
