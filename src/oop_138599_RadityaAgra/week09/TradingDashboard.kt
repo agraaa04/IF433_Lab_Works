94044373afce8f006550376a896243fd3076092e
RAAGRA <radityaagra045@gmail.com>
2026-05-14 22:03:45 +0700

    week09: (task) populate mock trade history list

---
+++ src/oop_138599_RadityaAgra/week09/TradingDashboard.kt
@@ -0,0 +1,13 @@
+package oop_138599_RadityaAgra.week09
+
+fun main() {
+    val tradeHistory = listOf(
+        TradeLog("BTCUSDT", "LONG", 20, 15.5, "CLOSED"),
+        TradeLog("ETHUSDT", "SHORT", 10, -5.2, "CLOSED"),
+        TradeLog("BTCUSDT", "SHORT", 25, 30.0, "CLOSED"),
+        TradeLog("ETHUSDT", "LONG", 15, -12.0, "CLOSED"),
+        TradeLog("SOLUSDT", "LONG", 10, 5.0, "OPEN"),
+        TradeLog("BTCUSDT", "LONG", 20, -2.5, "CLOSED")
+    )
+
+}
